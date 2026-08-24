acaf1429c3a839c915f5d2f02711ad6ddac97300
jakeclayton10 <joc2917@uncw.edu>
2026-08-24 12:14:20 -0400

    Add files via upload

---
+++ First Assignment.R
@@ -0,0 +1,22 @@
+# Swirl Lesson 1
+
+install.packages("swirl") # Install only once
+library(swirl) # Do once before every session
+install_course_github("isaaccloh","377_swirl") #Install only once
+
+# Starting up Swirl
+swirl()
+claytonj
+2
+1
+1
+1 # Questions to open up the assignments
+
+5+7 # Q1
+x = 5+7
+x <- 5+7 #q2 setting variable name
+
+x # viewing variable
+
+y <- x-3 # new variable y
+y #viewing y
