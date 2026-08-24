fbeaadaab61cf54c7e3609ba12f2ccba4a3979cb
jakeclayton10 <joc2917@uncw.edu>
2026-08-24 17:35:01 -0400

    Add files via upload

---
+++ Swirl_1.R
@@ -0,0 +1,38 @@
+# Swirl Lesson 1
+
+install.packages("swirl") # Install only once
+library(swirl) # Do once before every session
+install_course_github("isaaccloh","377_swirl") #Install only once
+
+# Starting up Swirl
+swirl()
+claytonj
+2
+1
+1
+1 # Questions to open up the assignments
+
+5+7 # Q1
+x = 5+7
+x <- 5+7 #q2 setting variable name
+
+x # viewing variable
+
+y <- x-3 # new variable y
+y #viewing y
+z <- c(1.1,9,3.14) #Setting this vector to a variable called z
+?c # Learning more about the concatenate command with a ?
+z #Viewing the vector we just created
+
+c(z,555,z) # New Vector containing the previous one
+z*2+1000 # Using z to do arithmetic, then changing the line
+my_sqrt <- sqrt(z-1) # Taking the square root of the vector
+3
+my_sqrt # viewing sqrt
+my_div <- z/my_sqrt #dividing vector by vector
+2 
+my_div
+c(1,2,3,4)+c(0,10)
+c(1,2,3,4)+c(0,10,100) # Learning how recylcing with vectors works
+my_div # using tab to find variables
+1
